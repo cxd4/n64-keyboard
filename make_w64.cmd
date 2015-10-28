8ec822497f1e8a969cd854363d03f7575c7a2b95
unknown <rj150@hotmail.com>
2015-10-28 17:29:16 -0400

    Make it easier to test 32-bit builds.

--- make_w64.cmd
+++ make_w64.cmd
@@ -1,11 +1,11 @@
 @ECHO OFF
 TITLE Windows Driver Kit 7.1.0
 
+    set target=amd64
 REM set target=i386
-set target=amd64
 
 set DDK=C:\WinDDK\7600.16385.1
-set MSVC=%DDK%\bin\x86\%target%
+set MSVC=%DDK%\bin\x86\amd64
 set incl=/I"%DDK%\inc\crt" /I"%DDK%\inc\api"
 set libs=/LIBPATH:"%DDK%\lib\crt\%target%"
 
